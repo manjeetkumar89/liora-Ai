6781a05ea1c72d460b7a3c58ed3b963bcceefa0a
manjeetkumar89 <scit403@gmail.com>
2025-09-26 02:47:25 +0530

    depyoing the project with api updation and frontend backend integration

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,16 +4,22 @@ const authRoutes = require('./routes/auth.routes');
 const chatRoutes = require('./routes/chat.routes');
 const cors = require('cors');
 const app = express();
+const path = require('path');
 
 app.use(cors({
-    origin:'http://localhost:4173',
+    origin:'http://localhost:5173',
     credentials:true
 })); // Enable CORS for all routes
 app.use(express.json());
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '../public')));
 
 
 app.use('/api/auth', authRoutes);
 app.use('/api/chat', chatRoutes);
+
+app.get('*name', (req,res)=>{
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+})
 
 module.exports = app;
