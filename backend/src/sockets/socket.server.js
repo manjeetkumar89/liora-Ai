765c185faa96ebe8c040bcde43a3cb3914ce02d4
manjeetkumar89 <scit403@gmail.com>
2026-03-23 16:13:40 +0530

    solving socket cors error

--- backend/src/sockets/socket.server.js
+++ backend/src/sockets/socket.server.js
@@ -12,7 +12,8 @@ const {createMemory, queryMemory} = require('../services/vector.service');
 function initSocketServer(httpServer){
     const io = new Server(httpServer,{
         cors : {
-            origin : "http://localhost:5173", //frontend url
+            origin:'https://liora-ai-chi.vercel.app',
+            //origin : "http://localhost:5173", //frontend url
             credentials : true
         }
     });
